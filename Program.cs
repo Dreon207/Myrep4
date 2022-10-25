5c14309e186238ff820d6d1f209d82c94126a679
Dreon207 <114486506+Dreon207@users.noreply.github.com>
2022-10-25 14:25:52 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Практическая_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rando = new Random();
+            int i = rando.Next(100);
+            int prob = 1;
+            Console.WriteLine("Загадано число от 1 до 100, у тебя всего одна попытка;)");
+            Console.WriteLine("Введите число ");
+            int k = Convert.ToInt32(Console.ReadLine());
+            while (prob <= 20)
+            {
+                if (i == k)
+                {
+                    Console.WriteLine("Хорошая работа, ты угадал");
+                    break;
+                }
+                else
+                {
+                    prob++;
+                    {
+                        Console.WriteLine("К сожелению ты не смог угадать");
+                        break;
+                    }
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Program2.cs
@@ -0,0 +1,25 @@
+﻿namespace Практическая_3._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var table = new int[11, 11];
+            for (int b = 1; b < 11; ++b)
+            {
+                for (int a = 1; a < 11; ++a)
+                {
+                    table[b, a] = b * a;
+                }
+            }
+            for (int b = 1; b < 11; ++b)
+            {
+                for (int a = 1; a < 11; ++a)
+                {
+                    Console.Write("{0, 3}", table[b, a]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
